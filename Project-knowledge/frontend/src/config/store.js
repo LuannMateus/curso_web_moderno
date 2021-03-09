8d2fb1334a5b3baf6d1bcda7cd230545bc0de394
LuannMateus <luanmateuslima34@gmail.com>
2021-03-08 23:57:20 -0300

    Add Home page with Dashboard

---
+++ Project-knowledge/frontend/src/config/store.js
@@ -0,0 +1,26 @@
+import Vue from 'vue';
+import Vuex from 'vuex';
+
+Vue.use(Vuex);
+
+export default new Vuex.Store({
+  state: {
+    isMenuVisible: true,
+    user: {
+      name: 'Mock User',
+      email: 'mock@cod34.com'
+    }
+  },
+  mutations: {
+    toggleMenu(state, isVisible) {
+      if (isVisible === undefined) {
+        state.isMenuVisible = !state.isMenuVisible;
+      } else {
+        state.isMenuVisible = isVisible
+      }
+
+
+    }
+  }
+})
+
--- Project-knowledge/frontend/src/main.js
+++ Project-knowledge/frontend/src/main.js
@@ -1,9 +1,20 @@
-import Vue from 'vue'
+import Vue from 'vue';
 
-import App from './App'
+import 'font-awesome/css/font-awesome.css';
+import './config/bootstrap';
+
+import store from './config/store';
+import router from './config/router';
+
+
+import App from './App';
 
 Vue.config.productionTip = false
 
+require('axios').defaults.headers.common['Authorization'] = process.env.VUE_APP_AUTH_TOKEN
+
 new Vue({
+  store,
+  router,
   render: h => h(App)
 }).$mount('#app')
